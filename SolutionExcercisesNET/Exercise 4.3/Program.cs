6c7609f5ee754b5ff91d24517dd4abc77ad84bad
cahoyos <cahoyos@tecnoquimicas.com>
2021-08-18 21:53:20 -0500

    Collections exercise 4.3

---
+++ SolutionExcercisesNET/Exercise 4.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise_4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityNumbers = 0;
+            Console.WriteLine("Enter 10 numbers: ");
+            while (true)
+            {
+                try
+                {
+                    quantityNumbers++;
+                    var input = Console.ReadLine();
+                    
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Wrong number! ");
+                    if (quantityNumbers != 0) quantityNumbers--;
+                    //throw;
+                }
+            }
+        }
+    }
+}
