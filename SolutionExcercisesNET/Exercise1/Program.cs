039a2f8906dacfe0c3325edca451d8bba55d4f8a
cahoyos <cahoyos@tecnoquimicas.com>
2021-08-18 19:32:44 -0500

    Collections exercise 1

---
+++ SolutionExcercisesNET/Exercise1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new ArrayList();
+
+            for (int i = 1; i <= 1000000; i++)
+            {
+                array.Add(i);
+            }
+
+            long sum = 0;
+            for (int i = 0; i < array.Count; i++)
+            {
+                sum += (int)array[i];
+                Console.WriteLine("Position: " + i + " Value: " + (int)array[i] + " Sum: " + sum);
+            }
+
+            //Console.WriteLine(sum);
+        }
+    }
+}
