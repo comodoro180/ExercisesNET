d045c37015a9ae52f2d63729f203681a2f56faf6
Carlos A. Hoyos <carlos.arturo.hoyos@gmail.com>
2021-08-18 22:42:46 -0500

    Collections exercise 4.3

--- SolutionExcercisesNET/Exercise 4.3/Program.cs
+++ SolutionExcercisesNET/Exercise 4.3/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+/*
+Pedir por pantalla 10 números, que iremos almacenando en un Listado (o 2 como prefiramos). 
+Al terminar de introducir los números, tendremos que recorrer el/los listados, diciéndonos 
+cuanto suman los pares y cuando los impares.
+*/
 
 namespace Exercise_4._3
 {
@@ -7,21 +13,38 @@ namespace Exercise_4._3
         static void Main(string[] args)
         {
             int quantityNumbers = 0;
+            List<int> numberList = new List<int>();
+
             Console.WriteLine("Enter 10 numbers: ");
             while (true)
             {
+                if (quantityNumbers == 10) break;
                 try
                 {
                     quantityNumbers++;
-                    var input = Console.ReadLine();
-                    
+                    string input = Console.ReadLine();                    
+                    numberList.Add(Convert.ToInt32(input));                    
                 }
                 catch (Exception)
                 {
                     Console.WriteLine("Wrong number! ");
-                    if (quantityNumbers != 0) quantityNumbers--;
-                    //throw;
+                    if (quantityNumbers != 0) quantityNumbers--;                    
                 }
+            }
+
+            int oddSum = 0;
+            int evenSum = 0;
+
+            foreach (var number in numberList)
+            {
+                if (number%2 == 0) oddSum += number;
+                else evenSum += number;
+            }
+            
+            if (numberList.Count > 0)
+            {
+                Console.WriteLine("Sum of pairs: " + evenSum);
+                Console.WriteLine("Sum of odds : " + oddSum);
             }
         }
     }
