b9ad78ec347d790adb2bdfa0b96c53e732b4565b
cahoyos <cahoyos@tecnoquimicas.com>
2021-08-18 21:23:44 -0500

    Collections exercise 4.2

---
+++ SolutionExcercisesNET/Exercise 4.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+Pedir por pantalla 5 nombres, e insertarlo en una Lista (List) una vez insertados, 
+se pedirá que introduzcamos un nombre, el programa tendrá que decirnos, si ese 
+nombre esta contenido en la lista.
+*/
+
+namespace Exercise_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namesList = new List<string>();
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Enter a name: ");
+                namesList.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter a word for search:");
+            string nameToFind = Console.ReadLine();
+            if (namesList.Contains(nameToFind)) Console.WriteLine("Name found!");
+            else Console.WriteLine("Name not found!");
+        }
+    }
+}
